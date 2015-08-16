e16314a363c7f9de64167660ac7d75fe30ebd202
Matthew Earl <matthew.earl@gmail.com>
2015-08-16 18:37:08 +0100

    Add post info template

---
+++ js/reddit-scores.js
@@ -0,0 +1,41 @@
+
+var reddit_scores = (function () {
+    var fetch = function() {
+        var id = ("t3_" +
+                  document.getElementById('reddit-url').href.split('/')[6])
+        var url = "http://www.reddit.com/api/info.json?id=" + id;
+        var req = new XMLHttpRequest();
+        var on_json_get = function(parsed) {
+            var score = parsed["data"]["children"][0]["data"]["score"];
+            var num_comments =
+                         parsed["data"]["children"][0]["data"]["num_comments"];
+            var text = "" + score + " points / " + num_comments + " comments.";
+            var divs = document.getElementsByClassName('reddit-score');
+            
+            console.log(divs.length);
+            for (var i = 0; i < divs.length; i++) {
+                var div = divs[i];
+                console.log(div);
+                while (div.firstChild) {
+                    div.removeChild(div.firstChild);
+                }
+                div.appendChild(document.createTextNode(text));
+            }
+        }
+        req.onreadystatechange = function() {
+            if (req.readyState == 4 && req.status == 200) {
+                var parsed = JSON.parse(req.responseText);
+                on_json_get(parsed);
+            }
+        }
+        req.open("get", url, true);
+        req.send();
+    }
+
+    return {
+        'fetch': fetch,
+    }
+})();
+
+reddit_scores.fetch();
+
